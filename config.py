e07af918b203723e36548e3c0ba95b3fd2d4dad8
Uday <110768804+UdayScripts@users.noreply.github.com>
2025-05-15 12:32:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,8 @@
-# © Cyril C Thomas
-# https://t.me/cyril_c_10
-
-import os
-
 class Config(object):
-    BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-    API_ID = int(os.environ.get("APP_ID", ))
-    API_HASH = os.environ.get("API_HASH", "")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -100))
-    MONGODB_URI = os.environ.get("MONGODB_URI", "")
-    SESSION_NAME = os.environ.get("SESSION_NAME", "")
-    BOT_OWNER = int(os.environ.get("BOT_OWNER", ))
+    BOT_TOKEN = "7524046996:AAFUjKHdCDo1C9ZJSrw3sTA8N1Q_zGbq48E"
+    API_ID = 26383754  # Replace with your actual API ID
+    API_HASH = "f743596f09f383e7bbcc62ce62367f06"
+    LOG_CHANNEL = -1001818844179  # Replace with your actual log channel ID
+    MONGODB_URI = "mongodb+srv://udaynath381:JSO2CTtPa7Fbqg88@data.ydg64.mongodb.net/songbot?retryWrites=true&w=majority&appName=data"
+    SESSION_NAME = "songbot"
+    BOT_OWNER = 1489381549  # Replace with your Telegram user ID
