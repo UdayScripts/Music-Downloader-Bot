f6efaf9d484f9421515b45ff37bdc389251f1386
Uday <110768804+UdayScripts@users.noreply.github.com>
2025-05-15 12:57:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ class Config(object):
     API_ID = 26383754  # Replace with your actual API ID
     API_HASH = "f743596f09f383e7bbcc62ce62367f06"
     LOG_CHANNEL = -1001818844179  # Replace with your actual log channel ID
-    MONGODB_URI = "mongodb+srv://udaynath381:JSO2CTtPa7Fbqg88@data.ydg64.mongodb.net/songbot?retryWrites=true&w=majority&appName=data"
+    MONGODB_URI = "mongodb+srv://udaynath381:JSO2CTtPa7Fbqg88@data.ydg64.mongodb.net/?retryWrites=true&w=majority&appName=data"
     SESSION_NAME = "songbot"
     BOT_OWNER = 1489381549  # Replace with your Telegram user ID
